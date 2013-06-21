1d7441108241d6fd8f921d05530a2a33b812b02b
Lucas <metallucas.ds@gmail.com>
2013-06-21 19:29:24 -0300

    Update...

--- app/scripts/views/slides.js
+++ app/scripts/views/slides.js
@@ -1,6 +1,20 @@
 define(['backbone'], function(Backbone) {
     var SlidesView = Backbone.View.extend({
+        el: $('slides'),
 
+        initialize: function() {
+            this.renderAll();
+        },
+
+        renderAll: function() {
+            this.$el.empty();
+            this.collection.each(this.render, this);
+        },
+
+        render: function(slide) {
+            var slideView = new SlideView({ model: slide });
+            this.$el.append(slideView.render().el);
+        }
     });
 
     return SlidesView;
