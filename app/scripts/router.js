39093e9b59bd662d3f5c10d410bc1b4a7f61b82a
Lucas <metallucas.ds@gmail.com>
2013-06-23 10:17:19 -0300

    Update router

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,5 +1,13 @@
 define(['backbone'], function(Backbone) {
   var Main = Backbone.Router.extend({
+    routes: {
+      '': 'home'
+    },
 
+    home: function() {
+      alert('Home route');
+    }
   });
+
+  return Main;
 });
